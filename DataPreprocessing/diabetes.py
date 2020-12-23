054ba9aae155a1777a79cfe2b1ff35905c3584e3
Choe-Yujun <cyj7732@gmail.com>
2020-12-23 16:28:30 +0900

    preprocessing

--- DataPreprocessing/diabetes.py
+++ DataPreprocessing/diabetes.py
@@ -1,6 +1,6 @@
-from sklearn.datasets import load_diabetes
+from sklearn import datasets, linear_model
 import pandas as pd
-from sklearn import linear_model
+from sklearn.metrics import mean_squared_error, r2_score
 import numpy as np
 from sklearn.preprocessing import OrdinalEncoder
 from sklearn.preprocessing import OneHotEncoder
@@ -8,42 +8,57 @@ from sklearn.preprocessing import MinMaxScaler
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-diabetes = load_diabetes()
+#create diabetes dataframe
+diabetes = datasets.load_diabetes()
 """
 print(diabetes)
-
 print(diabetes.DESCR)
-
 print(diabetes.data)
-
 print(diabetes.feature_names)
-
 print(diabetes.target)
-
 #print(diabetes.target_names)
 """
 df = pd.DataFrame(data=diabetes.data, columns=diabetes.feature_names)
-
 df['target'] = diabetes.target
-print(df)
 
+"""
+#sex column onehot encoding
 sex_data = df[["sex"]]
-#print(len(sex_data))
 oh_encoder = OneHotEncoder()
 encoded_sex_data = oh_encoder.fit_transform(sex_data)
-#print(len(encoded_sex_data.toarray()[0:]))
+
 df = df.drop("sex", axis=1)
-print(encoded_sex_data.toarray()[1])
-#for i in range(len(encoded_sex_data.toarray())):
-#    df[i]["sex"] = encoded_sex_data.toarray()[i]
-#data = np.reshape(encoded_sex_data.toarray(), (2, -1))
-#print(data)
-#print(oh_encoder.categories_)
-
-
-"""
-#df = np.c_[encoded_sex_data.toarray(), df]
-df['sex'] = encoded_sex_data.toarray()[0:]
+print(encoded_sex_data.toarray())
+l1 = []
+for i in range(len(encoded_sex_data.toarray())):
+    l1.append(encoded_sex_data.toarray()[i])
+df["sex"]= l1
 print(df)
-print(type(encoded_sex_data.toarray()))
 """
+minmax_scaler = MinMaxScaler()
+df = minmax_scaler.fit_transform(df)
+print(df)
+
+diabetes_X = df[:, np.newaxis, 2]
+print(diabetes_X.shape)
+diabetes_X_train = diabetes_X[:-20]
+diabetes_X_test = diabetes_X[-20:]
+
+diabetes_y_train = diabetes.target[:-20]
+diabetes_y_test = diabetes.target[-20:]
+
+regr = linear_model.LinearRegression()
+regr.fit(diabetes_X_train, diabetes_y_train)
+
+diabetes_y_pred = regr.predict(diabetes_X_test)
+print('Coefficients: \n', regr.coef_)
+print("Mean squared error: %.2f" % mean_squared_error(diabetes_y_test, diabetes_y_pred))
+print("Variance score: %.2f" % r2_score(diabetes_y_test, diabetes_y_pred))
+
+plt.scatter(diabetes_X_test, diabetes_y_test, color='black')
+plt.plot(diabetes_X_test, diabetes_y_pred, color='blue', linewidth=3)
+
+plt.xticks(())
+plt.yticks(())
+
+plt.show()
